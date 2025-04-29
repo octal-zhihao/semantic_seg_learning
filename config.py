47d86d8cd2cb4b8d832515de4ecd6a15bffbb215
octal-zhihao <1187456954@qq.com>
2025-04-29 10:31:43 +0800

    feat: add wandb

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ NUM_CLASSES = 21  # VOC2012
 IMAGE_SIZE = (256, 256)
 VOC_ROOT = 'D:/CodeField/semantic_seg_learning/data/'
 LR = 1e-3
-MAX_EPOCHS = 50
+MAX_EPOCHS = 200
--- train.py
+++ train.py
@@ -4,19 +4,32 @@ from model_module import SegmentationModel
 from data_module import VOCDataModule
 import pytorch_lightning as pl
 from config import MAX_EPOCHS
-
+from pytorch_lightning.loggers import WandBLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
+import wandb
 def main():
     model = SegmentationModel()
     data_module = VOCDataModule()
-
+    wandb.init(project='segmentation_model', entity='octal-zhihao')
+    wandb_logger = WandBLogger(project='segmentation_model', log_model=True)
+    checkpoint_callback = ModelCheckpoint(
+        monitor='val_mIoU',
+        dirpath='mycheckpoints/',
+        filename='best-checkpoint',
+        save_top_k=1,
+        mode='min'
+    )
     trainer = pl.Trainer(
         max_epochs=MAX_EPOCHS,
         accelerator='gpu' if torch.cuda.is_available() else 'cpu',
         precision='16-mixed' if torch.cuda.is_available() else 32,
-        log_every_n_steps=10
+        log_every_n_steps=10,
+        logger=wandb_logger,
+        callbacks=[checkpoint_callback],
     )
 
     trainer.fit(model, data_module)
+    wandb.finish()
 
 if __name__ == '__main__':
     main()
